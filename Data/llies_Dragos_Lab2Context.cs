8761f25e3e6b72b9f2d4a8e911a8fa4a6ee78927
Dragos <ilies.dragos2002@yahoo.com>
2023-10-22 13:13:00 +0300

    Add project files.

---
+++ Data/llies_Dragos_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using llies_Dragos_Lab2.Models;
+
+namespace llies_Dragos_Lab2.Data
+{
+    public class llies_Dragos_Lab2Context : DbContext
+    {
+        public llies_Dragos_Lab2Context (DbContextOptions<llies_Dragos_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<llies_Dragos_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<llies_Dragos_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<llies_Dragos_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace llies_Dragos_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public ICollection<Book>? Books { get; set; }
+    }
+}
